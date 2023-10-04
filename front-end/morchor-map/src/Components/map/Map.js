b97509da44475bf9fd528ac940b75da3e0813ebd
Qaizx <kaothom@gmail.com>
2023-10-05 01:46:04 +0700

    update

--- front-end/morchor-map/src/Components/map/Map.js
+++ front-end/morchor-map/src/Components/map/Map.js
@@ -1,143 +1,104 @@
 import "./Map.css";
 import "leaflet/dist/leaflet.css";
 
-import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import { MapContainer, Marker, Popup, TileLayer, useMap } from "react-leaflet";
 import L from "leaflet";
 import { useEffect, useState } from "react";
+import "https://unpkg.com/leaflet-routing-machine@latest/dist/leaflet-routing-machine.js";
+import Routing from "./Routing";
 
-const markerIcon = new L.Icon({
+const icon = L.icon({
   iconUrl: "../../img/location-pin.png",
   iconSize: [38, 38],
 });
 
-const Map = () => {
-  const [inputs, setInputs] = useState();
-  const [search, setSearch] = useState(false);
-
-  const handleChange = (event) => {
-    // if(inputs.name == '') setSearch(false)
-    // console.log(inputs);
-    const name = event.target.name;
-    const value = event.target.value;
-    setInputs(value);
-  
-  };
+const ResetCenterView = (props) => {
+  const { selectPosition } = props;
+  const map = useMap();
 
   useEffect(() => {
-    render()
-  }, [search])
-
-  const markers = [
-    {
-      id: 0,
-      geocode: [18.795612015074767, 98.95285065288007],
-      popUp: "ตึก 30 ปี",
-    },
-    {
-      id: 1,
-      geocode: [18.801588909598827, 98.95336653591599],
-      popUp: "SCB 1",
-    },
-    {
-      id: 2,
-      geocode: [18.797201944092684, 98.95183596448591],
-      popUp: "หอ 5 ชาย",
-    },
-  ];
-
-  const render = () => {
-    if(search){
-      return (
-        <>
-        {
-          Search(inputs)
-        }
-        </>
-      );
-    }else{
-      return (
-        <>
-          {MarkerMap()}
-        </>
+    if (selectPosition) {
+      map.setView(
+        L.latLng(selectPosition?.lat, selectPosition?.lon),
+        map.getZoom(),
+        { animate: true }
       );
     }
+  }, [selectPosition]);
+
+  return null;
+};
+
+const markers = [
+  {
+    id: 0,
+    geocode: [18.795612015074767, 98.95285065288007],
+    popUp: "ตึก 30 ปี",
+  },
+  {
+    id: 1,
+    geocode: [18.801588909598827, 98.95336653591599],
+    popUp: "SCB 1",
+  },
+  {
+    id: 2,
+    geocode: [18.797201944092684, 98.95183596448591],
+    popUp: "หอ 5 ชาย",
+  },
+];
+
+const render = (latitude, longitude) => {
+  return (
+    <MapContainer center={[latitude, longitude]} zoom={16}>
+      <TileLayer
+        url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+      />
+
+      // marker
+      {markers.map((marker) => (
+        <Marker position={marker.geocode} icon={icon}>
+          <Popup>
+            <h2>{marker.popUp}</h2>
+          </Popup>
+        </Marker>
+      ))}
+
+      // get user position
+      <Marker position={[latitude, longitude]} icon={icon}>
+        <Popup>
+          User Location
+        </Popup>
+      </Marker>
+
+      //get routing
+      <Routing/>
+    </MapContainer>
+  );
+};
+
+// useEffect(() => {
+//   render()
+// })
+
+const Map = (props) => {
+  const [latitude, setLatitude] = useState();
+  const [longitude, setLongitude] = useState();
+  const [fetch, setFetch] = useState(false);
+
+  navigator.geolocation.getCurrentPosition(showPosition);
+  const { selectPosition } = props;
+  const locationSelection = [selectPosition?.lat, selectPosition?.lon];
+
+  async function showPosition(po) {
+    setLatitude(po.coords.latitude);
+    setLongitude(po.coords.longitude);
+    setFetch(true);
   }
 
-  const MarkerMap = () => {
-    return (
-      <>
-        {markers.map((marker) => (
-          <Marker position={marker.geocode} icon={markerIcon}>
-            <Popup>
-              <h2>{marker.popUp}</h2>
-            </Popup>
-          </Marker>
-        ))}
-      </>
-    );
-  };
-
-  const handleSubmit = () => {
-    setSearch(true)
-  };
-
-  const handleClear = () => {
-    setSearch(false)
-    setInputs("")
+  if (fetch) {
+    return <>{render(latitude, longitude)}</>;
   }
-
-  const Search = (name) => {
-    const item = markers.filter(mar => mar.popUp === name)
-    return (
-      <>
-      {
-        item.map(item => (
-          <Marker position={item.geocode} icon={markerIcon}>
-            <Popup>
-              <h2>{item.popUp}</h2>
-            </Popup>
-          </Marker>
-        ))
-      }
-      </>
-    );
-  };
-
-  const BasicMap = () => {
-    return (
-      <>
-        <div className="formBlock">
-          <form id="form">
-            <input
-              type="text"
-              className="input"
-              id="start"
-              placeholder="Search..."
-              name="name"
-              value={inputs}
-              onChange={handleChange}
-            ></input>
-            <text onClick={handleSubmit} >Click here!</text>
-            <text>        </text>
-            <text onClick={handleClear} >Reset here!</text>
-          </form>
-        </div>
-
-        <div>
-          <MapContainer center={[18.8082, 98.9547]} zoom={16}>
-            <TileLayer
-              url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
-              attribution='&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-            />
-            {render()}
-
-          </MapContainer>
-        </div>
-      </>
-    );
-  };
-
-  return <div>{BasicMap()}</div>;
 };
 
 export default Map;
---
+++ front-end/morchor-map/src/Components/map/SearchBox.js
@@ -0,0 +1,95 @@
+import React, { useState } from "react";
+import {
+  List,
+  Button,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  Divider,
+} from "@mui/material";
+
+const NOMINATIM_BASE_URL = "https://nominatim.openstreetmap.org/search?";
+const params = {
+  q: "",
+  format: "json",
+  addressdetails: "addressdetails",
+};
+
+export default function SearchBox(props) {
+  const { selectPosition, setSelectPosition } = props;
+  const [searchText, setSearchText] = useState("");
+  const [listPlace, setListPlace] = useState([]);
+
+  return (
+    <>
+      <div className="formBlock">
+        <form id="form">
+          <input
+            type="text"
+            className="input"
+            id="start"
+            placeholder="Search..."
+            value={searchText}
+            onChange={(event) => {
+              setSearchText(event.target.value);
+            }}
+          />
+          <Button
+            variant="contained"
+            color="primary"
+            style={{ top: "10px" }}
+            onClick={() => {
+              // Search
+              const params = {
+                q: searchText,
+                format: "json",
+                addressdetails: 1,
+                polygon_geojson: 0,
+              };
+              const queryString = new URLSearchParams(params).toString();
+              const requestOptions = {
+                method: "GET",
+                redirect: "follow",
+              };
+              fetch(`${NOMINATIM_BASE_URL}${queryString}`, requestOptions)
+                .then((response) => response.text())
+                .then((result) => {
+                  setListPlace(JSON.parse(result));
+                })
+                .catch((err) => console.log("err: ", err));
+            }}
+          >
+            Search
+          </Button>
+
+          <div>
+            <List component="nav" aria-label="main mailbox folders">
+              {listPlace.map((item) => {
+                return (
+                  <div key={item?.place_id}>
+                    <ListItem
+                      button
+                      onClick={() => {
+                        setSelectPosition(item);
+                      }}
+                    >
+                      <ListItemIcon>
+                        <img
+                          src="./placeholder.png"
+                          alt="Placeholder"
+                          style={{ width: 38, height: 38 }}
+                        />
+                      </ListItemIcon>
+                      <ListItemText primary={item?.display_name} />
+                    </ListItem>
+                    <Divider />
+                  </div>
+                );
+              })}
+            </List>
+          </div>
+        </form>
+      </div>
+    </>
+  );
+}
