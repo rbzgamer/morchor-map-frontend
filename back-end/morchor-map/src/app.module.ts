91328f5f7689adc19b143baabbf52c36d3f816f8
rbzgamer <kuro123520@hotmail.com>
2023-10-05 23:45:57 +0700

    delete all unnecessary module and create some general method for location

--- back-end/morchor-map/src/app.module.ts
+++ back-end/morchor-map/src/app.module.ts
@@ -2,10 +2,6 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
-import { AdminModule } from './admin/admin.module';
-import { AddressModule } from './address/address.module';
-import { CategoryModule } from './category/category.module';
 import { LocationModule } from './location/location.module';
 
 @Module({
@@ -13,10 +9,6 @@ import { LocationModule } from './location/location.module';
     MongooseModule.forRoot(
       'mongodb+srv://admin:DJxawOWvdsdBnn4y@finalproject.0siimhv.mongodb.net/',
     ),
-    UsersModule,
-    AdminModule,
-    AddressModule,
-    CategoryModule,
     LocationModule,
   ],
   controllers: [AppController],
--- back-end/morchor-map/src/location/model/location.model.ts
+++ back-end/morchor-map/src/location/model/location.model.ts
@@ -1,16 +1,56 @@
 import * as mongoose from 'mongoose';
 
 export const LocationSchema = new mongoose.Schema({
-  name: String,
-  img: String,
-  addrId: Number,
-  catId: Number,
+  locationName: {
+    type: [String],
+    require: true,
+  },
+  categoryName: {
+    type: String,
+    require: true,
+  },
+  img: {
+    type: String,
+  },
+  houseNumber: {
+    type: Number,
+  },
+  villageNumber: {
+    type: Number,
+  },
+  alley: {
+    type: String,
+  },
+  subDistrict: {
+    type: String,
+  },
+  district: {
+    type: String,
+  },
+  postalCode: {
+    type: String,
+  },
+  latitude: {
+    type: String,
+    require: true,
+  },
+  longtitude: {
+    type: String,
+    require: true,
+  },
 });
 
 export interface Location {
-  id: number;
-  name: string;
+  id: string;
+  locationName: string[];
+  categoryName: string;
   img: string;
-  addrId: number;
-  catId: number;
+  houseNumber: number;
+  villageNumber: number;
+  alley: string;
+  subDistrict: string;
+  district: string;
+  postalCode: string;
+  latitude: string;
+  longtitude: string;
 }
