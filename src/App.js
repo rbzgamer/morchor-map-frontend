4f66ddba340285289449a8a9a825f26f9d7a2371
Qaizx <kaothom@gmail.com>
2024-03-09 17:40:52 +0700

    rebuild file

--- src/App.js
+++ src/App.js
@@ -22,8 +22,11 @@ export default function App() {
   const [choose, setChoose] = useState("Faculty");
   const [selectFaculty, setSelectFaculty] = useState("");
   const [selectBuilding, setSelectBuilding] = useState("");
-  const [latitudeFromUser, setLatitudeFromUser] = useState();
-  const [longitudeFromUser, setLongitudeFromUser] = useState();
+  {
+    /* default 30 years building engineer */
+  }
+  const [latitudeFromUser, setLatitudeFromUser] = useState(18.7956431);
+  const [longitudeFromUser, setLongitudeFromUser] = useState(98.9527902);
   const [fetched, setFetched] = useState(false);
   const [latitudeFromLocation, setLatitudeFromLocation] = useState();
   const [longitudeFromLocation, setLongitudeFromLocation] = useState();
@@ -54,11 +57,18 @@ export default function App() {
     setFetched(true);
   }
 
+  async function error() {
+    Swal.fire({
+      title: "Please enable GPS",
+      icon: "error",
+    });
+  }
+
   useEffect(() => {
-    navigator.geolocation.getCurrentPosition(showPosition);
+    navigator.geolocation.getCurrentPosition(showPosition, error);
   }, []);
 
-  if (fetched) {
+  const showMapInformation = () => {
     let latitude = 0.0;
     let longitude = 0.0;
     let userOrLocation = "user";
@@ -78,183 +88,278 @@ export default function App() {
 
     return (
       <>
-        <div className="application">
-          {/* Base Map */}
-          <div style={{ height: "100vh", width: "100%" }}>
-            <APIProvider apiKey={process.env.REACT_APP_GOOGLE_MAP_API_KEY}>
-              <Map
-                center={{ lat: latitudeFromUser, lng: longitudeFromUser }}
-                zoom={18}
-                mapId="d024e2b838dd693a"
-              >
-                {/*User location */}
-                <AdvancedMarker
-                  position={{ lat: latitudeFromUser, lng: longitudeFromUser }}
-                  onClick={() => {
-                    Swal.mixin({ toast: true }).fire({
-                      title: "User Location",
-                      icon: "info",
-                    });
-                  }}
-                >
-                  <img
-                    src="https://cdn-icons-png.flaticon.com/512/3603/3603850.png"
-                    width={50}
-                    height={50}
-                  />
-                </AdvancedMarker>
+        {/*User location */}
+        {userLocation()}
 
-                {/*Marker location */}
-                {!useRoute && (
-                  <MapCon
-                    latitude={latitude}
-                    longitude={longitude}
-                    userOrLocation={userOrLocation}
-                    submit={submit}
-                    selectFaculty={selectFaculty}
-                    setUseRoute={setUseRoute}
-                    setDestinationLat={setDestinationLat}
-                    setDestinationLng={setDestinationLng}
-                    isSearch={isSearch}
-                    nameSearch={nameSearch}
-                  />
-                )}
+        {/*Marker location */}
+        {markerLocation(latitude, longitude, userOrLocation)}
 
-                {/*Direction location */}
-                <Directions
-                  firstLocation={{ lat: originLat, lng: originLng }}
-                  secondLocation={{
-                    lat: destinationLat,
-                    lng: destinationLng,
-                  }}
-                  useRoute={useRoute}
-                  setUseRoute={setUseRoute}
-                  stateClickRoute={stateClickRoute}
-                  setMap={setMap}
-                  setDistance={setDistance}
-                  setDuration={setDuration}
-                />
-              </Map>
-            </APIProvider>
-          </div>
-          <div className="formBlock">
-            {/* Navbar */}
-            <Navbar
-              setResults={setResults}
-              setApprove={setApprove}
-              setSubmit={setSubmit}
-              setUseRoute={setUseRoute}
-              open={open}
-              setOpen={setOpen}
-              openDirectionBar={openDirectionBar}
-              setOpenDirectionBar={setOpenDirectionBar}
-              setChoose={setChoose}
-              setSelectFaculty={setSelectFaculty}
-              choose={choose}
-              setOriginName={setOriginName}
-              setDestinationName={setDestinationName}
-              latitudeFromUser={latitudeFromUser}
-              longitudeFromUser={longitudeFromUser}
-              setOriginLat={setOriginLat}
-              setOriginLng={setOriginLng}
-              setLatitudeFromLocation={setLatitudeFromLocation}
-              map={map}
-              setDistance={setDistance}
-              setDuration={setDuration}
-            />
-
-            {/* Route Bar */}
-            {openDirectionBar && (
-              <DirectionBar
-                originName={originName}
-                destinationName={destinationName}
-                setUseRoute={setUseRoute}
-                latitudeFromUser={latitudeFromUser}
-                longitudeFromUser={longitudeFromUser}
-                setOriginLat={setOriginLat}
-                setOriginLng={setOriginLng}
-                stateClickRoute={stateClickRoute}
-                setStateClickRoute={setStateClickRoute}
-              />
-            )}
-
-            {openDirectionBar && (
-              <ShowDirectionInfo distance={distance} duration={duration} />
-            )}
-            <div className="result-list">
-              {/* Search Result*/}
-              {approve && results && results.length > 0 && (
-                <Search
-                  searchData={results}
-                  setLatitudeFromLocation={setLatitudeFromLocation}
-                  setLongitudeFromLocation={setLongitudeFromLocation}
-                  setSubmit={setSubmit}
-                  setOriginName={setOriginName}
-                  setOriginLat={setOriginLat}
-                  setOriginLng={setOriginLng}
-                  setDestinationName={setDestinationName}
-                  setDestinationLat={setDestinationLat}
-                  setDestinationLng={setDestinationLng}
-                  setOpenDirectionBar={setOpenDirectionBar}
-                  open={open}
-                  map={map}
-                  setIsSearch={setIsSearch}
-                  setNameSearch={setNameSearch}
-                />
-              )}
-              {/* Category Result */}
-              {(choose === "Faculty" || choose === "") && !approve && (
-                <Faculty
-                  setChoose={setChoose}
-                  setSelectFaculty={setSelectFaculty}
-                  open={open}
-                />
-              )}
-              {/* Building Result */}
-              {!approve && choose === "Building" && (
-                <Building
-                  setChoose={setChoose}
-                  setSelectBuilding={setSelectBuilding}
-                  selectFaculty={selectFaculty}
-                  setLatitudeFromLocation={setLatitudeFromLocation}
-                  setLongitudeFromLocation={setLongitudeFromLocation}
-                  setSubmit={setSubmit}
-                  open={open}
-                  setOriginName={setOriginName}
-                  setOriginLat={setOriginLat}
-                  setOriginLng={setOriginLng}
-                  setDestinationName={setDestinationName}
-                  setDestinationLat={setDestinationLat}
-                  setDestinationLng={setDestinationLng}
-                  setOpenDirectionBar={setOpenDirectionBar}
-                  map={map}
-                />
-              )}
-              {/* Room Result */}
-              {!approve && choose === "Room" && (
-                <Room
-                  selectBuilding={selectBuilding}
-                  latitudeFromLocation={latitudeFromLocation}
-                  longitudeFromLocation={longitudeFromLocation}
-                  setSubmit={setSubmit}
-                  open={open}
-                  setOriginName={setOriginName}
-                  setOriginLat={setOriginLat}
-                  setOriginLng={setOriginLng}
-                  setDestinationName={setDestinationName}
-                  setDestinationLat={setDestinationLat}
-                  setDestinationLng={setDestinationLng}
-                  setOpenDirectionBar={setOpenDirectionBar}
-                />
-              )}
-            </div>
-          </div>
-        </div>
+        {/*Direction location */}
+        {directionOnMap()}
       </>
     );
-  } else {
+  };
+
+  const userLocation = () => {
     return (
-      <h1 className="noGPS">Please enable GPS</h1>
+      <AdvancedMarker
+        position={{ lat: latitudeFromUser, lng: longitudeFromUser }}
+        onClick={() => {
+          Swal.mixin({ toast: true }).fire({
+            title: "User Location",
+            icon: "info",
+          });
+        }}
+      >
+        <img
+          src="https://cdn-icons-png.flaticon.com/512/3603/3603850.png"
+          width={50}
+          height={50}
+        />
+      </AdvancedMarker>
     );
-  }
+  };
+
+  const markerLocation = (latitude, longitude, userOrLocation) => {
+    return (
+      <>
+        {!useRoute && (
+          <MapCon
+            latitude={latitude}
+            longitude={longitude}
+            userOrLocation={userOrLocation}
+            submit={submit}
+            selectFaculty={selectFaculty}
+            setUseRoute={setUseRoute}
+            setDestinationLat={setDestinationLat}
+            setDestinationLng={setDestinationLng}
+            isSearch={isSearch}
+            nameSearch={nameSearch}
+          />
+        )}
+      </>
+    );
+  };
+
+  const directionOnMap = () => {
+    return (
+      <Directions
+        firstLocation={{ lat: originLat, lng: originLng }}
+        secondLocation={{
+          lat: destinationLat,
+          lng: destinationLng,
+        }}
+        useRoute={useRoute}
+        setUseRoute={setUseRoute}
+        stateClickRoute={stateClickRoute}
+        setMap={setMap}
+        setDistance={setDistance}
+        setDuration={setDuration}
+      />
+    );
+  };
+
+  const showNavbar = () => {
+    return (
+      <div className="formBlock">
+        {/* Navbar */}
+        {searchNavbar()}
+
+        {/* Route Bar */}
+        {directionBar()}
+
+        {/* Result information */}
+        {resultList()}
+      </div>
+    );
+  };
+
+  const searchNavbar = () => {
+    return (
+      <Navbar
+        setResults={setResults}
+        setApprove={setApprove}
+        setSubmit={setSubmit}
+        setUseRoute={setUseRoute}
+        open={open}
+        setOpen={setOpen}
+        openDirectionBar={openDirectionBar}
+        setOpenDirectionBar={setOpenDirectionBar}
+        setChoose={setChoose}
+        setSelectFaculty={setSelectFaculty}
+        choose={choose}
+        setOriginName={setOriginName}
+        setDestinationName={setDestinationName}
+        latitudeFromUser={latitudeFromUser}
+        longitudeFromUser={longitudeFromUser}
+        setOriginLat={setOriginLat}
+        setOriginLng={setOriginLng}
+        setLatitudeFromLocation={setLatitudeFromLocation}
+        map={map}
+        setDistance={setDistance}
+        setDuration={setDuration}
+      />
+    );
+  };
+
+  const directionBar = () => {
+    return (
+      <>
+        {showOriginAndDestination()}
+        {showDirectionAndDuration()}
+      </>
+    );
+  };
+
+  const showOriginAndDestination = () => {
+    return (
+      <>
+        {openDirectionBar && (
+          <DirectionBar
+            originName={originName}
+            destinationName={destinationName}
+            setUseRoute={setUseRoute}
+            latitudeFromUser={latitudeFromUser}
+            longitudeFromUser={longitudeFromUser}
+            setOriginLat={setOriginLat}
+            setOriginLng={setOriginLng}
+            stateClickRoute={stateClickRoute}
+            setStateClickRoute={setStateClickRoute}
+          />
+        )}
+      </>
+    );
+  };
+
+  const showDirectionAndDuration = () => {
+    return (
+      <>
+        {openDirectionBar && (
+          <ShowDirectionInfo distance={distance} duration={duration} />
+        )}
+      </>
+    );
+  };
+
+  const resultList = () => {
+    return (
+      <div className="result-list">
+        {/* Search Result*/}
+        {searchInfo()}
+        {/* Category Result */}
+        {facultyInfo()}
+        {/* Building Result */}
+        {buildingInfo()}
+        {/* Room Result */}
+        {roomInfo()}
+      </div>
+    );
+  };
+
+  const searchInfo = () => {
+    return (
+      <>
+        {approve && results && results.length > 0 && (
+          <Search
+            searchData={results}
+            setLatitudeFromLocation={setLatitudeFromLocation}
+            setLongitudeFromLocation={setLongitudeFromLocation}
+            setSubmit={setSubmit}
+            setOriginName={setOriginName}
+            setOriginLat={setOriginLat}
+            setOriginLng={setOriginLng}
+            setDestinationName={setDestinationName}
+            setDestinationLat={setDestinationLat}
+            setDestinationLng={setDestinationLng}
+            setOpenDirectionBar={setOpenDirectionBar}
+            open={open}
+            map={map}
+            setIsSearch={setIsSearch}
+            setNameSearch={setNameSearch}
+          />
+        )}
+      </>
+    );
+  };
+
+  const facultyInfo = () => {
+    return (
+      <>
+        {(choose === "Faculty" || choose === "") && !approve && (
+          <Faculty
+            setChoose={setChoose}
+            setSelectFaculty={setSelectFaculty}
+            open={open}
+          />
+        )}
+      </>
+    );
+  };
+
+  const buildingInfo = () => {
+    return (
+      <>
+        {!approve && choose === "Building" && (
+          <Building
+            setChoose={setChoose}
+            setSelectBuilding={setSelectBuilding}
+            selectFaculty={selectFaculty}
+            setLatitudeFromLocation={setLatitudeFromLocation}
+            setLongitudeFromLocation={setLongitudeFromLocation}
+            setSubmit={setSubmit}
+            open={open}
+            setOriginName={setOriginName}
+            setOriginLat={setOriginLat}
+            setOriginLng={setOriginLng}
+            setDestinationName={setDestinationName}
+            setDestinationLat={setDestinationLat}
+            setDestinationLng={setDestinationLng}
+            setOpenDirectionBar={setOpenDirectionBar}
+            map={map}
+          />
+        )}
+      </>
+    );
+  };
+
+  const roomInfo = () => {
+    return (
+      <>
+        {!approve && choose === "Room" && (
+          <Room
+            selectBuilding={selectBuilding}
+            latitudeFromLocation={latitudeFromLocation}
+            longitudeFromLocation={longitudeFromLocation}
+            setSubmit={setSubmit}
+            open={open}
+            setOriginName={setOriginName}
+            setOriginLat={setOriginLat}
+            setOriginLng={setOriginLng}
+            setDestinationName={setDestinationName}
+            setDestinationLat={setDestinationLat}
+            setDestinationLng={setDestinationLng}
+            setOpenDirectionBar={setOpenDirectionBar}
+          />
+        )}
+      </>
+    );
+  };
+
+  return (
+    <div className="application">
+      {/* Base Map */}
+      <div style={{ height: "100vh", width: "100%" }}>
+        <APIProvider apiKey={process.env.REACT_APP_GOOGLE_MAP_API_KEY}>
+          <Map
+            center={{ lat: latitudeFromUser, lng: longitudeFromUser }}
+            zoom={18}
+            mapId="d024e2b838dd693a"
+          >
+            {fetched && showMapInformation()}
+            {fetched && showNavbar()}
+          </Map>
+        </APIProvider>
+      </div>
+    </div>
+  );
 }
